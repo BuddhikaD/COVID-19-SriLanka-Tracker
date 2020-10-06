72be15f2fc80ecbc8b8f15e2db54bc798a8c4ba4
BuddhikaD <buddhiextuse@gmail.com>
2020-10-06 08:25:07 +0530

    Updates

---
+++ precache-manifest.192fefc7bb5e78e51e4acd0f2773c4de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ed0cb025b64b0c8a03f1d4db645dd4",
+    "url": "/COVID-19-SriLanka-Tracker/index.html"
+  },
+  {
+    "revision": "1150253525ef9c4d7a25",
+    "url": "/COVID-19-SriLanka-Tracker/static/css/main.04d0ddc7.chunk.css"
+  },
+  {
+    "revision": "306d09b1cf654a26078e",
+    "url": "/COVID-19-SriLanka-Tracker/static/js/2.9f418175.chunk.js"
+  },
+  {
+    "revision": "6dea14eab83053243865ab6f2019f727",
+    "url": "/COVID-19-SriLanka-Tracker/static/js/2.9f418175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1150253525ef9c4d7a25",
+    "url": "/COVID-19-SriLanka-Tracker/static/js/main.b687d5dc.chunk.js"
+  },
+  {
+    "revision": "4225a42d3fb5d75bebf8",
+    "url": "/COVID-19-SriLanka-Tracker/static/js/runtime-main.814b703a.js"
+  }
+]);
